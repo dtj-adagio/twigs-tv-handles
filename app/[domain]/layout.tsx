723f1fb2eed7a1c56fc3da661e081210bd9a2d34
dtj-adagio <68013312+dtj-adagio@users.noreply.github.com>
2025-02-10 01:51:28 -0600

    Update layout.tsx
    Testing flex-2 after column1 was hidden

--- app/[domain]/layout.tsx
+++ app/[domain]/layout.tsx
@@ -21,7 +21,7 @@ export default function DomainLayout({ children, params }: Props) {
       <SiteHeader items={siteConfig.mainNav}>
         <MainNav title={params.domain} items={siteConfig.mainNav} />
       </SiteHeader>
-      <div className="flex flex-1 flex-row">
+      <div className="flex flex-2 flex-row">
         {children && (
           <div className="flex max-w-[980px] flex-col items-start gap-4">
             <iframe src="https://embed.twitch.tv/?channel=tofie&parent=twigs.tv" height="480px" width="854px" />
