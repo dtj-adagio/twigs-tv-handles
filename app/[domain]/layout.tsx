5267bb0a7a7c4e66f603cda6d31aa72096bdcb5e
dtj-adagio <68013312+dtj-adagio@users.noreply.github.com>
2025-02-10 02:13:49 -0600

    Update layout.tsx
    Adjusting max-w pixels and reverting flex-2 to flex-1

--- app/[domain]/layout.tsx
+++ app/[domain]/layout.tsx
@@ -21,10 +21,10 @@ export default function DomainLayout({ children, params }: Props) {
       <SiteHeader items={siteConfig.mainNav}>
         <MainNav title={params.domain} items={siteConfig.mainNav} />
       </SiteHeader>
-      <div className="flex flex-2 flex-wrap">
-        <div className="flex max-w-[980px] flex-col items-start gap-4">{children}</div>
-        <div className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-          <div className="flex max-w-[854px] flex-col items-end gap-4">
+      <div className="flex flex-1 flex-wrap">
+        <div className="flex max-w-[854px] flex-col items-start gap-4">{children}</div>
+        <div className="container grid items-top gap-6 pb-8 pt-6 md:py-10">
+          <div className="flex max-w-[854px] flex-col items-start gap-4">
             <iframe src="https://embed.twitch.tv/?channel=tofie&parent=twigs.tv" height="480px" width="854px"></iframe>
           </div>
         </div>
