d829cdbacacd86035788ae8164b2837df993113b
dtj-adagio <68013312+dtj-adagio@users.noreply.github.com>
2025-02-10 02:18:43 -0600

    Update layout.tsx
    Adding max-w to column2 container

--- app/[domain]/layout.tsx
+++ app/[domain]/layout.tsx
@@ -23,7 +23,7 @@ export default function DomainLayout({ children, params }: Props) {
       </SiteHeader>
       <div className="flex flex-1 flex-wrap">
         <div className="flex max-w-[854px] flex-col items-start gap-4">{children}</div>
-        <div className="container grid items-top gap-6 pb-8 pt-6 md:py-10">
+        <div className="container max-w-[854px] grid items-top gap-6 pb-8 pt-6 md:py-10">
           <div className="flex max-w-[854px] flex-col items-start gap-4">
             <iframe src="https://embed.twitch.tv/?channel=tofie&parent=twigs.tv" height="480px" width="854px"></iframe>
           </div>
